8270349f132ea0a6a2c1f9cf112de3b29e7253a9
maxconway <conway.max1@gmail.com>
2013-04-10 19:56:09 +0100

    Rearranged
    Moved some code to import-GDMO
    made it more generic

--- data/import-GDMO.R
+++ data/import-GDMO.R
@@ -1,6 +1,14 @@
+GDMO=NULL
 for(file in list.files(path = './experiments/', pattern = '.*GDMO.*.log',full.names=FALSE)){
   data = read.delim(paste('./experiments/',file,sep=''),header=FALSE)
-  colnames(data)[1:9] = c('cputime','iterations','knockouts','maxsyn','minsyn','biomass','front','crowding','generation','population')
+  colnames(data)[1:9] = c('cputime','iterations','knockouts','maxsyn','minsyn','biomass','front','crowding','generation','pop')
   assign(make.names(sub('.log','',file,fixed=TRUE)),data)
-  rm(data)
+  switch(match(strsplit(file,'.',fixed=TRUE)[1], c('geo_m_react','geo_s_react','iaf1260-ac'))
+         data$strain='metallireducens',
+         data$strain='sulfurreducens',
+         data$strain='iaf1260_ac'
+  )
+  data$gentime=data$cputime-c(rep(0,g$pop[1]),head(data$cputime,-data$pop[1]))
+  data$soltime=data$gentime/data$pop
+  GDMO=rbind(GDMO,data)
 }
