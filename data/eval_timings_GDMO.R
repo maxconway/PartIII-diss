1e6be74f6bfde75ff388805dddc4ef91fd811527
maxconway <conway.max1@gmail.com>
2013-04-10 14:55:57 +0100

    Correct & improve
    Took out bias towards large pop sizes
    Makes use of FBA timings

--- data/eval_timings_GDMO.R
+++ data/eval_timings_GDMO.R
@@ -1,8 +1,11 @@
 # produce timing prediction accuracy plot
 source('./timing-prediction.R')
+GDMO = GDMO[,c('FBAtime','cputime','iterations','pop')]
+GDMO = unique(GDMO)
+#GDMO$strain=as.factor(GDMO$strain)
 GDMO = GDMO[sample(nrow(GDMO)),]
 GDMO.train = head(GDMO,nrow(GDMO)*0.9)
 GDMO.test = tail(GDMO,nrow(GDMO)*0.1)
-GDMO.fit = lm(cputime~pop*iterations*strain,GDMO.train)
-GDMO.pred$prediction=predict(GDMO.fit,newdata=GDMO.test)
-boxplot(prediction/cputime~strain,GDMO.pred)
+GDMO.fit = lm(cputime~pop*iterations*FBAtime,GDMO.train)
+GDMO.test$prediction=predict(GDMO.fit,newdata=GDMO.test)
+boxplot(prediction/cputime~FBAtime,GDMO.pred)
