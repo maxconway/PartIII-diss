12cb58c942c3cadad8b9aaf3260ae9b1840d7b5b
maxconway <conway.max1@gmail.com>
2013-04-10 10:03:14 +0100

    Reorganize
    R code now stored here

---
+++ data/import-all.R
@@ -0,0 +1,6 @@
+for(file in list.files(path = './experiments/', pattern = '.*GDMO.*.log',full.names=FALSE)){
+  data = read.delim(paste('./experiments/',file,sep=''),header=FALSE)
+  colnames(data)[1:9] = c('cputime','iterations','knockouts','maxsyn','minsyn','biomass','front','crowding','generation')
+  assign(make.names(sub('.log','',file,fixed=TRUE)),data)
+  rm(data)
+}
