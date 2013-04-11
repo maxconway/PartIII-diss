cc02b51f45ed8e27cfe4c30b88fc454da08663b5
maxconway <conway.max1@gmail.com>
2013-04-11 13:23:33 +0100

    Correct columns

--- data/import-GDMO.R
+++ data/import-GDMO.R
@@ -1,7 +1,7 @@
 GDMO=NULL
 for(file in list.files(path = './experiments/', pattern = '.*GDMO.*.log',full.names=FALSE)){
   data = read.delim(paste('./experiments/',file,sep=''),header=FALSE)
-  colnames(data) = c('cputime','iterations','knockouts','maxsyn','minsyn','biomass','front','crowding','generation','pop','V12','id')
+  colnames(data) = c('cputime','walltime','iterations','knockouts','maxsyn','minsyn','biomass','front','crowding','generation','pop','id','unknown')
   switch(match(strsplit(file,'-GDMO',fixed=TRUE)[1], c('geo_m_react','geo_s_react','iaf1260-ac')),
          data$strain <- 'metallireducens',
          data$strain <- 'sulfurreducens',
