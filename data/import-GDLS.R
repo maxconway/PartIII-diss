4ea5de1d79763e18b0f384dd446192a1c3882e24
maxconway <conway.max1@gmail.com>
2013-04-11 15:37:31 +0100

    added more columns

--- data/import-GDLS.R
+++ data/import-GDLS.R
@@ -1,7 +1,7 @@
 GDLS=NULL
 for(file in list.files(path = './experiments/', pattern = '.*GDLS.*.log',full.names=FALSE)){
   data = read.delim(paste('./experiments/',file,sep=''),header=FALSE)
-  cols = c('cputime','walltime','iterations','minsyn','maxsyn','biomass','knockouts')
+  cols = c('cputime','walltime','iterations','minsyn','maxsyn','biomass','id','nbhdsz','maxknock','knockouts')
   colnames(data)[1:7] = cols
   data=data[,cols]
   if(startsWith(file,'iaf1260-ac')) data$strain<-'iaf1260-ac'
