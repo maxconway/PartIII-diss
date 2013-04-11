18658b9b835da4c392d45f7479a0d01887cedca0
maxconway <conway.max1@gmail.com>
2013-04-11 14:45:30 +0100

    fixed strain assignment

--- data/import-GDLS.R
+++ data/import-GDLS.R
@@ -1,6 +1,11 @@
+GDLS=NULL
 for(file in list.files(path = './experiments/', pattern = '.*GDLS.*.log',full.names=FALSE)){
   data = read.delim(paste('./experiments/',file,sep=''),header=FALSE)
-  colnames(data)[1:6] = c('cputime','iterations','knockouts','maxsyn','minsyn','biomass')
-  assign(make.names(sub('.log','',file,fixed=TRUE)),data)
-  rm(data)
+  cols = c('cputime','walltime','iterations','minsyn','maxsyn','biomass','knockouts')
+  colnames(data)[1:7] = cols
+  data=data[,cols]
+  if(startsWith(file,'iaf1260-ac')) data$strain<-'iaf1260-ac'
+  if(startsWith(file,'geo_m_react')) data$strain<-'metallireducens'
+  if(startsWith(file,'geo_s_react')) data$strain<-'sulfurreducens'
+  GDLS=rbind(GDLS,data)
 }
--- data/import-GDMO.R
+++ data/import-GDMO.R
@@ -2,11 +2,9 @@ GDMO=NULL
 for(file in list.files(path = './experiments/', pattern = '.*GDMO.*.log',full.names=FALSE)){
   data = read.delim(paste('./experiments/',file,sep=''),header=FALSE)
   colnames(data) = c('cputime','walltime','iterations','knockouts','maxsyn','minsyn','biomass','front','crowding','generation','pop','id','unknown')
-  switch(match(strsplit(file,'-GDMO',fixed=TRUE)[1], c('geo_m_react','geo_s_react','iaf1260-ac')),
-         data$strain <- 'metallireducens',
-         data$strain <- 'sulfurreducens',
-         data$strain <- 'iaf1260_ac'
-  )
+  if(startsWith(file,'iaf1260-ac')) data$strain<-'iaf1260-ac'
+  if(startsWith(file,'geo_m_react')) data$strain<-'metallireducens'
+  if(startsWith(file,'geo_s_react')) data$strain<-'sulfurreducens'
   data$gentime=data$cputime-c(rep(0,data$pop[1]),head(data$cputime,-data$pop[1]))
   data$soltime=data$gentime/data$pop
   GDMO=rbind(GDMO,data)
