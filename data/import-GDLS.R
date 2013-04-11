7bf07f7cbac693a9daab079caf32084cffb78ae6
maxconway <conway.max1@gmail.com>
2013-04-11 18:06:31 +0100

    column changes

--- data/import-GDLS.R
+++ data/import-GDLS.R
@@ -1,11 +1,11 @@
 GDLS=NULL
 for(file in list.files(path = './experiments/', pattern = '.*GDLS.*.log',full.names=FALSE)){
   data = read.delim(paste('./experiments/',file,sep=''),header=FALSE)
-  cols = c('cputime','walltime','iterations','minsyn','maxsyn','biomass','id','nbhdsz','maxknock','knockouts')
-  colnames(data)[1:7] = cols
+  cols = c('cputime','walltime','iterations','minsyn','maxsyn','biomass','knockouts','id','nbhdsz','M')
+  colnames(data)[1:10] = cols
   data=data[,cols]
   if(startsWith(file,'iaf1260-ac')) data$strain<-'iaf1260-ac'
-  if(startsWith(file,'geo_m_react')) data$strain<-'metallireducens'
   if(startsWith(file,'geo_s_react')) data$strain<-'sulfurreducens'
+  if(startsWith(file,'geo_m_react')) data$strain<-'metallireducens'
   GDLS=rbind(GDLS,data)
 }
