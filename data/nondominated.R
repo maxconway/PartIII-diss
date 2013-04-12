5eb443cf99d7d5ffc0e5055d0d5dfad7c087d6fa
maxconway <conway.max1@gmail.com>
2013-04-12 20:33:56 +0100

    Tidying
    -renaming
    -comments
    -code now duplicated in methods. should probably decide between 2

--- data/nondominated.R
+++ data/nondominated.R
@@ -1,13 +1,27 @@
+dominates <- function(a,b){
+  # does a dominate b?
+  any(a>b)&!any(a<b)
+}
+
+dominators <- function(sol,frame){
+  # how many rows in frame dominate sol?
+  apply(frame,1,function(t){
+    dominates(t,sol)
+  })
+}
+
 nondominated <- function(x){
-  dominates <- function(a,b){
-    any(a>b)&!any(a<b)
-  }
-  dominators <- function(sol,frame){
-    apply(frame,1,function(t){
-      dominates(t,sol)
-    })
-  }
+  # which rows of x are nondominated?
+  # Returns:
+  #  boolean vector
   apply(x,1,function(a){
     !any(dominators(a,x))
   })
+}
+
+fnondominated <- function(df){
+  # which rows of x are nondominated?
+  # Returns:
+  #  data.frame
+  df[nondominated(df),]
 }
