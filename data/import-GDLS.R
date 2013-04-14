daecd5fbffaa6e6061ed4c94e67e1f0c324d3ea2
maxconway <conway.max1@gmail.com>
2013-04-14 20:38:48 +0100

    Discussed GDLS timing

--- data/import-GDLS.R
+++ data/import-GDLS.R
@@ -1,4 +1,5 @@
 GDLS=NULL
+require(gdata,quiet=TRUE)
 for(file in list.files(path = './experiments/', pattern = '.*GDLS.*.log',full.names=FALSE)){
   data = read.delim(paste('./experiments/',file,sep=''),header=FALSE)
   cols = c('cputime','walltime','iterations','minsyn','maxsyn','biomass','knockouts','id','nbhdsz','M')
@@ -9,3 +10,8 @@ for(file in list.files(path = './experiments/', pattern = '.*GDLS.*.log',full.na
   if(startsWith(file,'geo_m_react')) data$strain<-'metallireducens'
   GDLS=rbind(GDLS,data)
 }
+FBAtimings <- read.csv("./FBAtimings.csv")
+FBAtimings$FBAtime=FBAtimings$time
+FBAtime=aggregate(FBAtime~strain,FBAtimings[,c('strain','FBAtime')],mean)
+GDLS=merge(GDLS,FBAtime)
+save(GDLS,file='GDLS.RData')
