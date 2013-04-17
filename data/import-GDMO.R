8c67fc25c85eb8972cae14b538e91aec86061b4c
maxconway <conway.max1@gmail.com>
2013-04-17 23:21:19 +0100

    Refactoring, beginning of E.Coli

--- data/import-GDMO.R
+++ data/import-GDMO.R
@@ -3,13 +3,23 @@ GDMO=NULL
 for(file in list.files(path = './experiments/', pattern = '.*GDMO.*.log',full.names=FALSE)){
   data = read.delim(paste('./experiments/',file,sep=''),header=FALSE)
   colnames(data) = c('cputime','walltime','iterations','knockouts','maxsyn','minsyn','biomass','front','crowding','generation','pop','id','unknown')
+  #strains
   if(startsWith(file,'iaf1260-ac')) data$strain<-'iaf1260-ac'
   if(startsWith(file,'iJO1366_Ecoli_suc_aerobic')) data$strain<-'iJO1366-aerobic'
   if(startsWith(file,'iJO1366_Ecoli_suc_anaerobic')) data$strain<-'iJO1366-anaerobic'
   if(startsWith(file,'geo_m_react')) data$strain<-'metallireducens'
   if(startsWith(file,'geo_s_react')) data$strain<-'sulfurreducens'
+  #timings
   data$gentime=data$cputime-c(rep(0,data$pop[1]),head(data$cputime,-data$pop[1]))
   data$soltime=data$gentime/data$pop
+  #normalization
+  natural<-unique(data[data$knockouts==0,c('strain','biomass','maxsyn','minsyn')])
+  colnames(natural)<-c('strain','natbiomass','natmaxsyn','natminsyn')
+  data<-merge(data,natural)
+  data$nbiomass <- data$biomass/data$natbiomass
+  data$nmaxsyn <- data$maxsyn/data$natmaxsyn
+  data$nminsyn <- data$minsyn/data$natminsyn
+  #join
   GDMO=rbind(GDMO,data)
 }
 data('FBAtimings')
