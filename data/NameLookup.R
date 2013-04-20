d9d80926a86ae5bbda367a65f6876fcadad3cfa8
maxconway <conway.max1@gmail.com>
2013-04-20 17:23:17 +0100

    corrected lookup
    now uses a table, not a switch statment

--- data/NameLookup.R
+++ data/NameLookup.R
@@ -1,14 +1,13 @@
+to<-c('iaf1260-ac','iJO1366-aerobic','iJO1366-anaerobic','metallireducens','sulfurreducens')
+from<-c('iaf1260-ac','iJO1366_Ecoli_suc_aerobic','iJO1366_Ecoli_suc_anaerobic','geo_m_react','geo_s_react')
+glossary<-data.frame(to,from)
+
 NameLookup <- function(string){
-  switch(string,
-         iaf1260-ac='iaf1260-ac',
-         iJO1366_Ecoli_suc_aerobic='iJO1366-aerobic',
-         iJO1366_Ecoli_suc_anaerobic='iJO1366-anaerobic',
-         geo_m_react='metallireducens',
-         geo_s_react='sulfurreducens',
-         
-         iaf1260-ac='iaf1260-ac',
-         iJO1366-aerobic='iJO1366_Ecoli_suc_aerobic',
-         iJO1366-anaerobic='iJO1366_Ecoli_suc_anaerobic',
-         metallireducens='geo_m_react',
-         sulfurreducens='geo_s_react')
+  string<-as.character(string)
+if(string %in% glossary$to){
+  return(glossary[glossary$to==string,'from'])
+}
+if(string %in% glossary$from){
+  return(glossary[glossary$from==string,'t0'])
+}
 }
