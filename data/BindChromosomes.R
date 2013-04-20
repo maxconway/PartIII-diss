a19b41c226bf9d9005b47b8e45803b225b97498f
maxconway <conway.max1@gmail.com>
2013-04-20 17:24:16 +0100

    minor
    removed sources
    added protection
    added comma

--- data/BindChromosomes.R
+++ data/BindChromosomes.R
@@ -1,15 +1,12 @@
-source('./data/getNames.R',local=TRUE)
-source('./data/getChromosomes.R',local=TRUE)
-
 BindChromosomes <- function(x){
   id=unique(x$id)
-  genepattern=switch(unique(x$strain),
+  genepattern<-switch(as.character(unique(x$strain)),
                      sulfurreducens='GSU.*',
                      metallireducens='Gmet_.*')#check
-  names=getNames(paste0(NameLookup(unique(x$strain))'.reduced'))
+  names=getNames(paste0(NameLookup(unique(x$strain)),'.reduced'))
   x.chromosomes=getChromosomes(id)
   colnames(x.chromosomes)<-c((names),c('maxsyn','minsyn','biomass','front','crowding','?'))
-  x.chromosomes[,grepl('GSU.*',colnames(x.chromosomes))]<-x.chromosomes[,grepl(genepattern,colnames(x.chromosomes))]==1
+  x.chromosomes[,grepl(pattern=genepattern,colnames(x.chromosomes))]<-x.chromosomes[,grepl(genepattern,colnames(x.chromosomes))]==1
   x.chromosomes<-cbind(x.chromosomes,x[,c('knockouts','nmaxsyn','nminsyn','nbiomass')])
   #deduplicate
   x.chromosomes<-unique(x.chromosomes)
